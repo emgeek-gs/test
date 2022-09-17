a136a34a73ee78dc6fca2975552eedc5ea1cfa95
emgeek-gs <52862178+emgeek-gs@users.noreply.github.com>
2022-09-17 14:20:34 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    
+    entry: {
+        main: path.join(__dirname, './webapp/js/main.js')
+    },
+    
+    output: {
+        path: path.join(__dirname, './build'),
+        filename: "[name].js",
+        sourceMapFilename: "[name].js.map"
+        // publicPath: "/build/"
+    },
+    
+    devtool: "source-map",
+
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        'scss': [
+                            'vue-style-loader',
+                            'css-loader',
+                            'sass-loader'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        query: {
+                            presets: ['@babel/env']
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    {
+                        loader: "vue-style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg|jpg|gif)$/, 
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 131072
+                    }
+                } 
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new LiveReloadPlugin(),
+        new webpack.NormalModuleReplacementPlugin(/(.*)polyfills\/node\/(.*)/, function(resource) {
+            resource.request = resource.request.replace(/polyfills\/node\//, `polyfills\/web\/`);
+        }),
+        new CopyPlugin({
+          patterns: [
+            { from: 'webapp/public', to: '' }
+          ]
+        })
+    ],
+
+    node: {
+        fs: 'empty'
+    },
+
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            'ddb': path.resolve(__dirname, 'vendor/ddb_js')
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+
+    externals: {
+    }
+}
